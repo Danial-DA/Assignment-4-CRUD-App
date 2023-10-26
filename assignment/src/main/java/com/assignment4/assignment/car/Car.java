66dd0b242b9ced8a58d6a07637430ff5626c0ac1
Danial <d_afzal@uncg.edu>
2023-10-26 15:30:53 -0400

    progress

--- assignment/src/main/java/com/assignment4/assignment/car/Car.java
+++ assignment/src/main/java/com/assignment4/assignment/car/Car.java
@@ -27,11 +27,13 @@ public class Car {
 
     private String make;
     private String model;
+    private int year;
     private double price;
 
-    public Car(String make, String model, double price) {
+    public Car(String make, String model, int year, double price) {
         this.make = make;
         this.model = model;
+        this.year = year;
         this.price = price;
     }
 
